f0b9493235c443d03b859b9b0aa8a849465f34c4
Richard Lems <richard@q42.nl>
2017-04-30 12:49:39 +0200

    get template data from a json file

--- js/data.js
+++ js/data.js
@@ -1,15 +1,34 @@
 $(document).ready(function() {
-
-  var data = { projects:
-        [
-          {title:"mazda", subTitle:"svg car loader", tag1:"html", tag2:"svg"},
-          {title:"lessonup", subTitle:"motion prototyping", tag1:"html", tag2:"svg"},
-          {title:"scubabear", subTitle:"ludum dare game", tag1:"gamejam", tag2:"construct2", tag3:"SCUBA"},
-          {title:"marioheader", subTitle:"konami code", tag1:"construct 2", tag2:"easter egg"}
-        ]};
-
-  var templateScript = $("#project-template").html();
-  var template = Handlebars.compile (templateScript);
-  $("#poop").append (template (data));
-
+  getData();
 });
+
+function getData(){
+
+  $.getJSON( "js/data.json", function( data ) {
+
+    var source   = $("#project-template").html();
+    var template = Handlebars.compile(source);
+
+    $('#poop').append(template(data));
+  });
+
+  //
+  // templatedata = {projects:
+  //       [
+  //         {title:"mazda", subTitle:"svg car loader", tag1:"html", tag2:"svg"},
+  //         {title:"lessonup", subTitle:"motion prototyping", tag1:"html", tag2:"svg"},
+  //         {title:"scubabear", subTitle:"ludum dare game", tag1:"gamejam", tag2:"construct2", tag3:"SCUBA"},
+  //         {title:"marioheader", subTitle:"konami code", tag1:"construct 2", tag2:"easter egg"}
+  //       ]};
+
+
+
+  // var data = { projects:
+  //       [
+  //         {title:"mazda", subTitle:"svg car loader", tag1:"html", tag2:"svg"},
+  //         {title:"lessonup", subTitle:"motion prototyping", tag1:"html", tag2:"svg"},
+  //         {title:"scubabear", subTitle:"ludum dare game", tag1:"gamejam", tag2:"construct2", tag3:"SCUBA"},
+  //         {title:"marioheader", subTitle:"konami code", tag1:"construct 2", tag2:"easter egg"}
+  //       ]};
+
+}
